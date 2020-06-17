fe4ce40ddfab47802e62f27b69637bda747979a0
Roman Kuchinik <kvroma83@gmail.com>
2020-06-17 23:25:37 +0300

    Added binding to the fields of the "Material Accessories" and "Stairs" table

--- Builders/ViewModels/GeneratedViewModel.cs
+++ Builders/ViewModels/GeneratedViewModel.cs
@@ -13,7 +13,7 @@ namespace Builders.ViewModels
         private BuilderContext db;        
         private Generated generatedSelect;
 
-        #region Material Detail Properti
+        #region Material Detail Property
         private DIC_G_GradeLevel levelDetailSelect;
         private List<DIC_G_GradeLevel> levelDetails;
         private DIC_G_Partition partitionDetailSelect;
@@ -246,14 +246,329 @@ namespace Builders.ViewModels
             }
         }
         #endregion
+        #region Material Accessories Property
+        private DIC_G_AccessoriesFloor floorNameAccessorieSelect;
+        private List<DIC_G_AccessoriesFloor> floorNameAccessories;
+        private DIC_G_TypeAccessoriesFloor floorTypeAccessorieSelect;
+        private List<DIC_G_TypeAccessoriesFloor> floorTypeAccessories;
+        private string totalMaterialAccessorie;
+        private GeneratedAccessories generatedAccessorieSelect;
+        private List<GeneratedAccessories> generatedAccessories;
 
+        public DIC_G_AccessoriesFloor FloorNameAccessorieSelect
+        {
+            get => floorNameAccessorieSelect; 
+            set
+            {
+                floorNameAccessorieSelect = value;
+                OnPropertyChanged(nameof(FloorNameAccessorieSelect));
+                if (FloorNameAccessorieSelect != null)
+                {
+                    FloorTypeAccessories = db.DIC_G_TypeAccessoriesFloors.Where(t=>t.DIC_G_AccessoriesFloorId == FloorNameAccessorieSelect.Id).ToList();
+                }
+            }
+        }
+        public List<DIC_G_AccessoriesFloor> FloorNameAccessories
+        {
+            get => floorNameAccessories; 
+            set
+            {
+                floorNameAccessories = value;
+                OnPropertyChanged(nameof(FloorNameAccessories));
+            }
+        }
+        public DIC_G_TypeAccessoriesFloor FloorTypeAccessorieSelect
+        {
+            get => floorTypeAccessorieSelect; 
+            set
+            {
+                floorTypeAccessorieSelect = value;
+                OnPropertyChanged(nameof(FloorTypeAccessorieSelect));
+            }
+        }
+        public List<DIC_G_TypeAccessoriesFloor> FloorTypeAccessories
+        {
+            get => floorTypeAccessories; 
+            set
+            {
+                floorTypeAccessories = value;
+                OnPropertyChanged(nameof(FloorTypeAccessories));
+            }
+        }
+        public string TotalMaterialAccessorie
+        {
+            get => totalMaterialAccessorie; 
+            set
+            {
+                totalMaterialAccessorie = value;
+                OnPropertyChanged(nameof(TotalMaterialAccessorie));
+            }
+        }
+        public GeneratedAccessories GeneratedAccessorieSelect
+        {
+            get => generatedAccessorieSelect; 
+            set
+            {
+                generatedAccessorieSelect = value;
+                OnPropertyChanged(nameof(GeneratedAccessorieSelect));
+            }
+        }
+        public List<GeneratedAccessories> GeneratedAccessories
+        {
+            get => generatedAccessories; 
+            set
+            {
+                generatedAccessories = value;
+                OnPropertyChanged(nameof(GeneratedAccessories));
+            }
+        }                
+        #endregion        
+        #region Stairs Property
+        private DIC_G_GradeLevel levelStairSelect;
+        private List<DIC_G_GradeLevel> levelStairs;
+        private DIC_G_TypeStairs typeStairSelect;
+        private List<DIC_G_TypeStairs> typeStairs;
+        private decimal qtyStairs;
+        private decimal lenghtStairs;
+        private string demolitionStairSelect;
+        private List<string> demolitionStairs;
+        private DIC_G_TypeLeveling typeLevelingStairSelect;
+        private List<DIC_G_TypeLeveling> typeLevelingStairs;
+        private decimal qtyLevelingStairs;
+        private GeneratedStairs generatedStairSelect;
+        private List<GeneratedStairs> generatedStairs;
+
+        public DIC_G_GradeLevel LevelStairSelect
+        {
+            get => levelStairSelect; 
+            set
+            {
+                levelStairSelect = value;
+                OnPropertyChanged(nameof(LevelStairSelect));
+            }
+        }
+        public List<DIC_G_GradeLevel> LevelStairs
+        {
+            get => levelStairs; 
+            set
+            {
+                levelStairs = value;
+                OnPropertyChanged(nameof(LevelStairs));
+            }
+        }
+        public DIC_G_TypeStairs TypeStairSelect
+        {
+            get => typeStairSelect; 
+            set
+            {
+                typeStairSelect = value;
+                OnPropertyChanged(nameof(TypeStairSelect));
+            }
+        }
+        public List<DIC_G_TypeStairs> TypeStairs
+        {
+            get => typeStairs; 
+            set
+            {
+                typeStairs = value;
+                OnPropertyChanged(nameof(TypeStairs));
+            }
+        }
+        public decimal QtyStairs
+        {
+            get => qtyStairs; 
+            set
+            {
+                qtyStairs = value;
+                OnPropertyChanged(nameof(QtyStairs));
+            }
+        }
+        public decimal LenghtStairs
+        {
+            get => lenghtStairs; 
+            set
+            {
+                lenghtStairs = value;
+                OnPropertyChanged(nameof(LenghtStairs));
+            }
+        }
+        public string DemolitionStairSelect
+        {
+            get => demolitionStairSelect; 
+            set
+            {
+                demolitionStairSelect = value;
+                OnPropertyChanged(nameof(DemolitionStairSelect));
+            }
+        }
+        public List<string> DemolitionStairs
+        {
+            get => demolitionStairs; 
+            set
+            {
+                demolitionStairs = value;
+                OnPropertyChanged(nameof(DemolitionStairs));
+            }
+        }
+        public DIC_G_TypeLeveling TypeLevelingStairSelect
+        {
+            get => typeLevelingStairSelect; 
+            set
+            {
+                typeLevelingStairSelect = value;
+                OnPropertyChanged(nameof(TypeLevelingStairSelect));
+            }
+        }
+        public List<DIC_G_TypeLeveling> TypeLevelingStairs
+        {
+            get => typeLevelingStairs; 
+            set
+            {
+                typeLevelingStairs = value;
+                OnPropertyChanged(nameof(TypeLevelingStairs));
+            }
+        }
+        public decimal QtyLevelingStairs
+        {
+            get => qtyLevelingStairs; 
+            set
+            {
+                qtyLevelingStairs = value;
+                OnPropertyChanged(nameof(QtyLevelingStairs));
+            }
+        }
+        public GeneratedStairs GeneratedStairSelect
+        {
+            get => generatedStairSelect; 
+            set
+            {
+                generatedStairSelect = value;
+                OnPropertyChanged(nameof(GeneratedStairSelect));
+            }
+        }
+        public List<GeneratedStairs> GeneratedStairs
+        {
+            get => generatedStairs; 
+            set
+            {
+                generatedStairs = value;
+                OnPropertyChanged(nameof(GeneratedStairs));
+            }
+        }        
+        #endregion
+        #region Moldings Property
+        private DIC_G_Modeling accessoriesMoldingSelect;
+        private List<DIC_G_Modeling> accessoriesMoldings;
+        private DIC_G_TypeModeling typeMoldingSelect;
+        private List<DIC_G_TypeModeling> typeMoldings;
+        private decimal qtyMolding;
+        private decimal heightMolding;
+        private DIC_G_Painting paintingMoldingSelect;
+        private List<DIC_G_Painting> paintingMoldings;
+        private GeneratedMolding generatedMoldingSelect;
+        private List<GeneratedMolding> generatedMoldings;
+
+        public DIC_G_Modeling AccessoriesMoldingSelect
+        {
+            get => accessoriesMoldingSelect; 
+            set
+            {
+                accessoriesMoldingSelect = value;
+                OnPropertyChanged(nameof(AccessoriesMoldingSelect));
+            }
+        }
+        public List<DIC_G_Modeling> AccessoriesMoldings
+        {
+            get => accessoriesMoldings; 
+            set
+            {
+                accessoriesMoldings = value;
+                OnPropertyChanged(nameof(AccessoriesMoldings));
+            }
+        }
+        public DIC_G_TypeModeling TypeMoldingSelect
+        {
+            get => typeMoldingSelect; 
+            set
+            {
+                typeMoldingSelect = value;
+                OnPropertyChanged(nameof(TypeMoldingSelect));
+            }
+        }
+        public List<DIC_G_TypeModeling> TypeMoldings
+        {
+            get => typeMoldings; 
+            set
+            {
+                typeMoldings = value;
+                OnPropertyChanged(nameof(TypeMoldings));
+            }
+        }
+        public decimal QtyMolding
+        {
+            get => qtyMolding; 
+            set
+            {
+                qtyMolding = value;
+                OnPropertyChanged(nameof(QtyMolding));
+            }
+        }
+        public decimal HeightMolding
+        {
+            get => heightMolding; 
+            set
+            {
+                heightMolding = value;
+                OnPropertyChanged(nameof(HeightMolding));
+            }
+        }
+        public DIC_G_Painting PaintingMoldingSelect
+        {
+            get => paintingMoldingSelect; 
+            set
+            {
+                paintingMoldingSelect = value;
+                OnPropertyChanged(nameof(PaintingMoldingSelect));
+            }
+        }
+        public List<DIC_G_Painting> PaintingMoldings
+        {
+            get => paintingMoldings; 
+            set
+            {
+                paintingMoldings = value;
+                OnPropertyChanged(nameof(PaintingMoldings));
+            }
+        }
+        public GeneratedMolding GeneratedMoldingSelect
+        {
+            get => generatedMoldingSelect; 
+            set
+            {
+                generatedMoldingSelect = value;
+                OnPropertyChanged(nameof(GeneratedMoldingSelect));
+            }
+        }
+        public List<GeneratedMolding> GeneratedMoldings
+        {
+            get => generatedMoldings; 
+            set
+            {
+                generatedMoldings = value;
+                OnPropertyChanged(nameof(GeneratedMoldings));
+            }
+        }
+        #endregion
 
         public GeneratedViewModel(ref BuilderContext context, int id)
         {
             db = context;            
 
             LoadGenerator(id);
+            LoadComboBox();
             LoadDetail();
+            LoadAccessoriess();
+            LoadStairs();
         }
 
         private void LoadGenerator(int id)
@@ -271,9 +586,9 @@ namespace Builders.ViewModels
                 generatedSelect = db.Generateds.FirstOrDefault(g => g.ClientId == id);
             }
         }
-        private void LoadDetail()
+        private void LoadComboBox()
         {
-            GeneratedMaterials = db.GeneratedMaterials.Where(g=>g.GeneratedId == generatedSelect.Id).ToList();
+            // Details
             LevelDetails = db.DIC_G_GradeLevels.ToList();
             PartitionDetails = db.DIC_G_Partitions.ToList();
             AdditionalDetails = db.DIC_G_Additionals.ToList();
@@ -284,6 +599,28 @@ namespace Builders.ViewModels
             DemolitionDetails.Add("Yes");
             DemolitionDetails.Add("No");
             DemolitionDetailSelect = DemolitionDetails[1];
+            // Accessories
+            FloorNameAccessories = db.DIC_G_AccessoriesFloors.ToList();
+            // Stairs
+            LevelStairs = db.DIC_G_GradeLevels.ToList();
+            TypeStairs = db.DIC_G_TypeStairs.ToList();
+            DemolitionStairs = new List<string>();
+            DemolitionStairs.Add("Yes");
+            DemolitionStairs.Add("No");
+            DemolitionStairSelect = DemolitionStairs[1];
+            TypeLevelingStairs = db.DIC_G_TypeLevelings.ToList();
+        }
+        private void LoadDetail()
+        {
+            GeneratedMaterials = db.GeneratedMaterials.Where(g=>g.GeneratedId == generatedSelect.Id).ToList();            
+        }
+        private void LoadAccessoriess()
+        {
+            GeneratedAccessories = db.GeneratedAccessories.Where(g => g.GeneratedId == generatedSelect.Id).ToList();
+        }
+        private void LoadStairs()
+        {
+            GeneratedStairs = db.GeneratedStairs.Where(g => g.GeneratedId == generatedSelect.Id).ToList();
         }
     }
 }
